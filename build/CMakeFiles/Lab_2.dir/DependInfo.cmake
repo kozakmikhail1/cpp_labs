8f51c32e2296af9a5d34939345a8fb71208dd22e
user <kozak_mikhail.1@mail.ru>
2025-09-23 20:38:18 +0300

    git pricol

---
+++ build/CMakeFiles/Lab_2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Code/C++/cpp-labs/laboratory_work_2/src/array.cpp" "CMakeFiles/Lab_2.dir/src/array.cpp.obj" "gcc" "CMakeFiles/Lab_2.dir/src/array.cpp.obj.d"
+  "C:/Code/C++/cpp-labs/laboratory_work_2/src/functions.cpp" "CMakeFiles/Lab_2.dir/src/functions.cpp.obj" "gcc" "CMakeFiles/Lab_2.dir/src/functions.cpp.obj.d"
+  "C:/Code/C++/cpp-labs/laboratory_work_2/src/main.cpp" "CMakeFiles/Lab_2.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Lab_2.dir/src/main.cpp.obj.d"
+  "C:/Code/C++/cpp-labs/laboratory_work_2/src/menu.cpp" "CMakeFiles/Lab_2.dir/src/menu.cpp.obj" "gcc" "CMakeFiles/Lab_2.dir/src/menu.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Lab_2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_2.dir/src/array.cpp.obj"
+  "CMakeFiles/Lab_2.dir/src/array.cpp.obj.d"
+  "CMakeFiles/Lab_2.dir/src/functions.cpp.obj"
+  "CMakeFiles/Lab_2.dir/src/functions.cpp.obj.d"
+  "CMakeFiles/Lab_2.dir/src/main.cpp.obj"
+  "CMakeFiles/Lab_2.dir/src/main.cpp.obj.d"
+  "CMakeFiles/Lab_2.dir/src/menu.cpp.obj"
+  "CMakeFiles/Lab_2.dir/src/menu.cpp.obj.d"
+  "Lab_2.exe"
+  "Lab_2.exe.manifest"
+  "Lab_2.pdb"
+  "libLab_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
