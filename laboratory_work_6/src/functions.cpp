ee8be7471291e853a395c39acc6306676a1bfa0e
user <kozak_mikhail.1@mail.ru>
2025-10-05 12:39:20 +0300

    Fix function input_int

--- laboratory_work_6/src/functions.cpp
+++ laboratory_work_6/src/functions.cpp
@@ -3,18 +3,16 @@
 int input_int(const std::string &prompt = "Enter a single integer: ")
 {
     int value = 0;
-    while (true)
-    {
-        std::cout << C_YELLOW << prompt << C_WHITE;
-        std::cin >> value;
 
-        if (std::cin.good() && std::cin.peek() == '\n')
-        {
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            return value;
-        }
-        std::cin.clear();
+    std::cout << C_YELLOW << prompt << C_WHITE;
+    std::cin >> value;
+
+    if (std::cin.good() && std::cin.peek() == '\n')
+    {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        throw std::invalid_argument("You entered incorect value\n");
+        return value;
     }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    throw std::invalid_argument("You entered incorect value\n");
 }
